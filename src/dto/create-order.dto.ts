62e48ada8617ca261d32da8f0e4a2004a1de8095
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-02-28 01:49:22 -0400

    Documentation

--- src/dto/create-order.dto.ts
+++ src/dto/create-order.dto.ts
@@ -1,19 +1,51 @@
 import { IsString, IsArray, IsNotEmpty } from "class-validator";
 import { OrderItem } from "../interfaces/order-item.interfaces";
-
+import { Type } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 export class CreateOrderDto {
+  @ApiProperty({
+    description: "Items of the order",
+    example: [
+      { productId: "aswer44ddñpdochh9", quantity: 1 },
+      { productId: "jauueetcnzkoeyfg1525", quantity: 2 },
+    ],
+    required: true,
+  })
   @IsArray()
   @IsNotEmpty()
   items: OrderItem[];
 
+  @ApiProperty({
+    description: "Total of the order",
+    example: 10,
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  total: number;
+
+  @ApiProperty({
+    description: "Customer name",
+    example: "Juan Perez",
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   customerName: string;
 
+  @ApiProperty({
+    description: "Customer phone",
+    example: "123456789",
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   customerPhone: string;
-
+  @ApiProperty({
+    description: "Customer address",
+    example: "Calle 123",
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   customerAddress: string;
--- src/dto/create-products.dto.ts
+++ src/dto/create-products.dto.ts
@@ -1,9 +1,61 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+
 export class CreateProductsDto {
+  @ApiProperty({
+    description: "Name of the product",
+    example: "Pizza",
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  @Type(() => String)
   name: string;
+
+  @ApiProperty({
+    description: "Price of the product",
+    example: 10,
+    required: true,
+    minimum: 1,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @Type(() => Number)
   price: number;
-  idCategory: string;
+
+  @ApiProperty({
+    description: "Description of the product",
+    example: "Musrooms,Pepper,Onions",
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  @Type(() => String)
   description: string;
+
+  @ApiProperty({
+    description: "Stock of the product",
+    example: 10,
+    required: true,
+  })
+  @ApiProperty({
+    description: "Stock of the product",
+    example: 10,
+    required: true,
+  })
   stockIn: number;
-  stockOut: number;
+
+  @ApiProperty({
+    description: "Id Category ",
+    example: "sjjs8887djsahd73743278sdhsh",
+    required: true,
+  })
+  @IsNumber()
+  @Type(() => String)
   categoryId: string;
+  // idCategory: string;
+
+  stockOut: number;
+  
 }
