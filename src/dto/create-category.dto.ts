105f73c1044a380236fff6ed8287f5ad7e027286
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-02-28 02:02:09 -0400

    Doc Category

--- src/dto/create-category.dto.ts
+++ src/dto/create-category.dto.ts
@@ -1,6 +1,16 @@
 import { IsString, IsArray, IsNotEmpty } from "class-validator";
 import { OrderItem } from "../interfaces/order-item.interfaces";
+import { Type } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateCategoryDto {
+  @ApiProperty({
+    description: "Name of the category",
+    example: "Pizza Napoli",
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  @Type(() => String)
   name: string;
 }
--- src/dto/create-order.dto.ts
+++ src/dto/create-order.dto.ts
@@ -22,6 +22,7 @@ export class CreateOrderDto {
   })
   @IsString()
   @IsNotEmpty()
+  @Type(() => Number)
   total: number;
 
   @ApiProperty({
@@ -31,6 +32,7 @@ export class CreateOrderDto {
   })
   @IsString()
   @IsNotEmpty()
+  @Type(() => String)
   customerName: string;
 
   @ApiProperty({
@@ -40,6 +42,7 @@ export class CreateOrderDto {
   })
   @IsString()
   @IsNotEmpty()
+  @Type(() => String)
   customerPhone: string;
   @ApiProperty({
     description: "Customer address",
@@ -48,5 +51,6 @@ export class CreateOrderDto {
   })
   @IsString()
   @IsNotEmpty()
+  @Type(() => String)
   customerAddress: string;
 }
